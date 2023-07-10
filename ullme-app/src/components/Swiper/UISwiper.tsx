6d0962c7a06cf2e989551c254290ebb766a49679
Mikhail <mishakolode@gmail.com>
2023-07-10 15:46:56 +0300

    feat: ready web adaptiv

--- ullme-app/src/components/Swiper/UISwiper.tsx
+++ ullme-app/src/components/Swiper/UISwiper.tsx
@@ -2,6 +2,9 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import { Pagination } from "swiper/modules";
+import "./pagination.css";
+
 import s from "./UISwiper.module.scss";
 import cn from "classnames";
 
@@ -9,14 +12,48 @@ const UISwiper = () => {
   return (
     <>
       <Swiper
-        slidesPerView={'auto'}
+        // pagination={true}
+        modules={[Pagination]}
+        slidesPerView={"auto"}
         centeredSlides={true}
-        spaceBetween={8}
+        spaceBetween={window.innerWidth < 768 ? 8 : 12}
         loop={true}
+        pagination={{ clickable: true }}
         // className="mySwiper"
       >
+        <SwiperSlide>
+          <div className={cn(s.slideContainer, s.blueGray)}>
+            <div className={s.firstSlide}>
+              <p className={s.whiteText}>
+                Checking the similarity by{" "}
+                <span className={s.yellowText}>123 points</span> of facial
+                geometry with AI
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={cn(s.slideContainer, s.peach)}>
+            <div className={s.secondSlide}>
+              <p className={s.whiteText}>
+                Scientists speculate,{" "}
+                <span className={s.blueGrayText}>we check!</span>
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={cn(s.slideContainer, s.white)}>
+            <div className={s.firstSlide}>
+              <p className={s.blackText}>
+                <span className={s.redText}>Every minutes </span> someone checks
+                their likeness{" "}
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
 
-        <SwiperSlide>
+        {/* <SwiperSlide>
           <div className={cn(s.slideContainer, s.peach)}>
             <div className={s.secondSlide}>
               <p className={s.whiteText}>
@@ -46,41 +83,10 @@ const UISwiper = () => {
               </p>
             </div>
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={cn(s.slideContainer, s.peach)}>
-            <div className={s.secondSlide}>
-              <p className={s.whiteText}>
-                Scientists speculate,{" "}
-                <span className={s.blueGrayText}>we check!</span>
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={cn(s.slideContainer, s.white)}>
-            <div className={s.firstSlide}>
-              <p className={s.blackText}>
-                <span className={s.redText}>Every minutes </span> someone checks
-                their likeness{" "}
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={cn(s.slideContainer, s.blueGray)}>
-            <div className={s.firstSlide}>
-              <p className={s.whiteText}>
-                Checking the similarity by{" "}
-                <span className={s.yellowText}>123 points</span> of facial
-                geometry with AI
-              </p>
-            </div>
-          </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
       </Swiper>
     </>
   );
 };
 
-export default UISwiper
+export default UISwiper;
