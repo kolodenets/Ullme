0a1235a96c4e92bfad4a9daa55ad38e287fb49ec
Mikhail <mishakolode@gmail.com>
2023-07-17 10:37:48 +0300

    feat: new content and styles for desktop

--- ullme-app/src/pages/home/Home.tsx
+++ ullme-app/src/pages/home/Home.tsx
@@ -5,11 +5,11 @@ import Logo from "/assets/images/Logo.png";
 import Circle from "/assets/icons/Status-Normal.svg";
 import CircleWeb from "/assets/icons/Status-Normal-web.svg";
 import Percent from "/assets/icons/percent.svg";
-import MainImage from "/assets/images/image1-web.png";
+import MainImage from "/assets/images/mainImageWeb.png";
 import Parents from "/assets/images/parents.png";
 import ParentsMob from "/assets/images/parents-mob.png";
-import SecondImage from "../../../public/assets/images/image2-web.png";
-import SecondImageMobile from "../../../public/assets/images/image2.png";
+import SecondImage from "../../../public/assets/images/secondImageWeb.png";
+import SecondImageMobile from "../../../public/assets/images/secondImageMob.png";
 import ThirdImage from "../../../public/assets/images/image3-web.png";
 import s from "./Home.module.scss";
 import UISwiper from "../../components/Swiper/UISwiper";
@@ -24,18 +24,30 @@ const Home = () => {
         <div className={s.heroContent}>
           <div className={s.heroContainer}>
             <div className={s.heroBody}>
-              <h1 className={s.mainTitle}>Do you look alike?</h1>
-              <h4 className={s.mainSubtitle}>
-                Ullme AI service knows the answer!
-              </h4>
+              <h1 className={s.mainTitle}>ULLME</h1>
+              <h4 className={s.mainSubtitle}>AI service knows the answer!</h4>
               <p className={s.mainInfo}>
-                Scientists have proven that people who look like{" "}
-                {size.width > 768 && <br />} each other are attracted to each
-                other. It is laid down by nature. <br />
-                <br /> It's time to test this statement for yourself!{" "}
+                Is an artificial intelligence soft developed and trained on the
+                basis of many years of scientific research. <br />
                 {size.width > 768 && <br />}
-                Upload photos of your couple and find out how much you look
-                like.
+                Scientists have proven that people who look like each other are
+                attracted to each other. <br />
+                It is laid down by nature. <br /> {size.width > 768 && <br />}
+                You can read more here{" "}
+                <span className={s.svg}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                  >
+                    <path
+                      d="M20.8125 20.25C20.8125 20.3992 20.7532 20.5423 20.6477 20.6477C20.5423 20.7532 20.3992 20.8125 20.25 20.8125H3.75C3.60082 20.8125 3.45774 20.7532 3.35225 20.6477C3.24676 20.5423 3.1875 20.3992 3.1875 20.25C3.1875 20.1008 3.24676 19.9577 3.35225 19.8523C3.45774 19.7468 3.60082 19.6875 3.75 19.6875H20.25C20.3992 19.6875 20.5423 19.7468 20.6477 19.8523C20.7532 19.9577 20.8125 20.1008 20.8125 20.25ZM7.5 16.3125C7.64913 16.3124 7.79211 16.253 7.8975 16.1475L17.4375 6.60844V14.25C17.4375 14.3992 17.4968 14.5423 17.6023 14.6477C17.7077 14.7532 17.8508 14.8125 18 14.8125C18.1492 14.8125 18.2923 14.7532 18.3977 14.6477C18.5032 14.5423 18.5625 14.3992 18.5625 14.25V5.25C18.5625 5.10082 18.5032 4.95774 18.3977 4.85225C18.2923 4.74676 18.1492 4.6875 18 4.6875H9C8.85082 4.6875 8.70774 4.74676 8.60225 4.85225C8.49676 4.95774 8.4375 5.10082 8.4375 5.25C8.4375 5.39918 8.49676 5.54226 8.60225 5.64775C8.70774 5.75324 8.85082 5.8125 9 5.8125H16.6416L7.1025 15.3525C7.02393 15.4312 6.97044 15.5313 6.94878 15.6404C6.92711 15.7494 6.93825 15.8625 6.98078 15.9652C7.02332 16.0679 7.09534 16.1557 7.18775 16.2175C7.28016 16.2794 7.38882 16.3124 7.5 16.3125Z"
+                      fill="#1890FF"
+                    />
+                  </svg>
+                </span>
               </p>
               <Button
                 className={s.checkSimilarity}
@@ -48,52 +60,57 @@ const Home = () => {
         </div>
         <div className={s.mainImageContainer}>
           <img src={MainImage} alt="mainImage" className={s.mainImage} />
-          <div className={s.percentage}>
+          {/* <div className={s.percentage}>
             <img src={Percent} alt="percent" className={s.percentImg} />
-          </div>
+          </div> */}
         </div>
       </section>
-      <UISwiper />
+      <div className={s.textblock}>
+        <p className={s.whiteText}>
+          Checking the similarity by{" "}
+          <span className={s.yellowText}>123 points</span> of facial geometry
+          with AI
+        </p>
+      </div>
       <section className={cn("container", s.secondSection)}>
         <div className={s.secondSection__contentContainer}>
-          <div className={s.secondSection__imageWrapper}>
-            <div className={s.secondSection__titleContainer}>
-              <h2 className={cn(s.h2, s.secondInfo__mobile)}>
-                When looking for love, start by looking in the mirror
-              </h2>
-              <div className={s.center}>
-                <p className={s.secondInfo}>
-                  Why Do So Many Couples Look Alike? Here’s the Psychology
-                  Behind the Weird Phenomenon
-                </p>
-              </div>
+          <div className={s.secondSection__titleContainer}>
+            <h2 className={cn(s.secondSection__title, s.h2, s.secondInfo__mobile)}>
+              When looking for love, start by looking in the mirror
+            </h2>
+            <div className={s.center}>
+              <p className={s.secondInfo}>
+                Why Do So Many Couples Look Alike? Here’s the Psychology Behind
+                the Weird Phenomenon
+              </p>
             </div>
+          </div>
+          <div className={s.secondSection__imageWrapper}>
             <div className={s.secondImage}>
               <img
                 src={size.width < 768 ? SecondImageMobile : SecondImage}
                 alt="second-image"
               />
             </div>
-          </div>
-
-          <div
-            className={cn(s.longText, {
-              [s.regularText]: size.width > 768,
-              ["container"]: size.width < 768,
-            })}
-          >
-            <span>
-              Married couples often do not notice that they resemble each other.
-              They are told of this fact by their acquaintances. <br />
-              <br /> Lookalike couples have captured public fascination for
-              years. Back in 1987, scientists from the University of Michigan
-              set out to study the phenomenon of married couples who grow to
-              look more alike over time.
-              <br />
-              <br /> Their theory, which scientists still cite today, was that
-              decades of shared emotions result in a closer resemblance due to
-              similar wrinkles and expressions.
-            </span>
+            <div
+              className={cn(s.longText, {
+                [s.regularText]: size.width > 768,
+                ["container"]: size.width < 768,
+              })}
+            >
+              <span>
+                Married couples often do not notice that they resemble each
+                other. They are told of this fact by their acquaintances. <br />
+                <br /> Lookalike couples have captured public fascination for
+                years. Back in 1987, scientists from the University of Michigan
+                set out to study the phenomenon of married couples who grow to
+                look more alike over time.
+                <br />
+                <br /> Their theory, which scientists still cite today, was that
+                decades of shared emotions result in a closer resemblance due to
+                similar wrinkles and expressions.
+              </span>
+            </div>
           </div>
         </div>
 
@@ -109,9 +126,6 @@ const Home = () => {
         <div className={cn("container", s.thirdSection__contentContainer)}>
           <div className={s.thirdSection__wrapper}>
             <div className={s.thirdSection__titleContainer}>
-              <div className={s.logoImg}>
-                <img src={Logo} alt="logo" />
-              </div>
               <h2 className={cn(s.h2, s.likeness)}>
                 How is your likeness determined
               </h2>
